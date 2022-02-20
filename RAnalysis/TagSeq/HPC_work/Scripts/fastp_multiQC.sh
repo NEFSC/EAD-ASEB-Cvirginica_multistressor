259ad92a82c7bb72965ff3ef71b2aaad2c69df9a
Samuel Gurr <samuel.gurr>
2022-02-20 17:58:49 -0500

    scripts update 20220220

--- RAnalysis/TagSeq/HPC_work/Scripts/fastp_multiQC.sh
+++ RAnalysis/TagSeq/HPC_work/Scripts/fastp_multiQC.sh
@@ -2,22 +2,24 @@
 #SBATCH -t 120:00:00
 #SBATCH --nodes=1 --ntasks-per-node=20
 #SBATCH --mem=100GB
+#SBATCH --account=putnamlab
 #SBATCH --export=NONE
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=samuel.gurr@noaa.gov
+#SBATCH --mail-user=samuel_gurr@uri.edu
 #SBATCH --output="%x_out.%j"
 #SBATCH --error="%x_err.%j"
-#SBATCH -D /sgurr/Cvirginica_multistressor_TagSeq/output/fastp_multiQC
+#SBATCH -D /data/putnamlab/sgurr/Geoduck_TagSeq/output/fastp_multiQC
 
 # load modules needed
-module load bio/fastp/0.23.2
-module load bio/fastqc/0.11.9 
+module load fastp/0.19.7-foss-2018b
+module load FastQC/0.11.8-Java-1.8
+module load MultiQC/1.7-foss-2018b-Python-2.7.15
 
 # symbolically link 'clean' reads to hisat2 dir
-ln -s ../../../../../share/nefsc/mcfarland_sequecenes/TagSeq_oysters_2021/SA21200*/*.fastq.gz ./ # call backward from the directory to the share folder, input symbolic link to directory (-D in SBATCH header)
+ln -s ../../../../KITT/hputnam/20201217_Geoduck_TagSeq/*.fastq.gz ./
 
 # Make an array of sequences to trim
-array1=($(ls SA21200*/*.fastq.gz)) 
+array1=($(ls *.fastq.gz)) 
 
 # fastp loop; trim the Read 1 TruSeq adapter sequence; trim poly x default 10 (to trim polyA) 
 for i in ${array1[@]}; do
@@ -28,8 +30,6 @@ done
 echo "Read trimming of adapters complete." $(date)
 
 # Quality Assessment of Trimmed Reads
-
-source ../../../python_venv/bin/activate # from the current directory (-D in SBATCH header), activates the bin of installed python packages, including multiqc
 
 multiqc ./ #Compile MultiQC report from FastQC files
 
