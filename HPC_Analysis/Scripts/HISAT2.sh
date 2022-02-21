8dde531697a7238db46c1b1a1dd8ebb1c695f0dd
Samuel Gurr <samuel.gurr>
2022-02-21 16:42:20 -0500

    added scripts from HPC

--- HPC_Analysis/Scripts/HISAT2.sh
+++ HPC_Analysis/Scripts/HISAT2.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
-#SBATCH -t 120:00:00
-#SBATCH --nodes=1 --ntasks-per-node=10
-#SBATCH --mem=200GB
-#SBATCH --account=putnamlab
-#SBATCH --export=NONE
-#SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=samuel_gurr@uri.edu
+#SBATCH --job-name="hisat2_align"
+#SBATCH -t 048:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=samuel.gurr@noaa.gov
 #SBATCH --output="%x_out.%j"
 #SBATCH --error="%x_err.%j"
-#SBATCH -D /data/putnamlab/sgurr/Geoduck_TagSeq/output/HISAT2
 
-#load packages
-module load HISAT2/2.1.0-foss-2018b #Alignment to reference genome: HISAT2
-module load SAMtools/1.9-foss-2018b #Preparation of alignment for assembly: SAMtools
+# before running..
+# make directory named hisat2 as output/hisat2/
 
-# symbolically link 'clean' reads to hisat2 dir
-ln -s ../fastp_multiQC/clean*.fastq.gz ./ 
+cd #nav back to home directory (allows job to be run from anywhere)
+
+# load modules, requires hisat2 and samtools
+module load bio/hisat2/2.2.1
+module load bio/samtools/1.11
+
+cd Cvirginica_multistressor_TagSeq/output/hisat2 # nav to hisat2 - symbolic directory works well when output to the current dir as ./
+
+# symbolically link clean reads to hisat2 dir
+ln -s ../fastp_multiQC/clean/*.fastq.gz ./ # call the .fastq.gz output from fastp trim - make symb link to output/hisat2
+
+# activate python fro hisat2-build
+source ../../../python_venv/bin/activate  # activate python cirtual envriomment to call python and run hisat2-build
 
 # index the reference genome for Panopea generosa output index to working directory
-hisat2-build -f ../../../refs/Panopea-generosa-v1.0.fa ./Pgenerosa_ref # called the reference genome (scaffolds)
+hisat2-build -f ../../../refs/GCF_002022765.2_C_virginica-3.0_genomic.fna ./Cvirginica_ref
 echo "Referece genome indexed. Starting alingment" $(date)
+
+# exit python virtual envrionment
+deactivate
 
 # This script exports alignments as bam files
 # sorts the bam file because Stringtie takes a sorted file for input (--dta)
 # removes the sam file because it is no longer needed
 array=($(ls *.fastq.gz)) # call the symbolically linked sequences - make an array to align
 for i in ${array[@]}; do
-        sample_name=`echo $i| awk -F [.] '{print $2}'`
-	hisat2 -p 8 --dta -x Pgenerosa_ref -U ${i} -S ${sample_name}.sam
-        samtools sort -@ 8 -o ${sample_name}.bam ${sample_name}.sam
-    		echo "${i} bam-ified!"
-        rm ${sample_name}.sam
+        hisat2 -p 8 --dta -x Cvirginica_ref -U ${i} -S ${i}.sam
+        samtools sort -@ 8 -o ${i}.bam ${i}.sam
+                echo "${i} bam-ified!"
+        rm ${i}.sam
 done
-
